373d18c398ea2574773c558594dd3f41f2132606
Sachin Thakur <44505980+2306raj@users.noreply.github.com>
2022-02-08 20:53:29 +0530

    Update __init__.py

--- octoprint_helloworld/__init__.py
+++ octoprint_helloworld/__init__.py
@@ -3,8 +3,9 @@ from __future__ import absolute_import, unicode_literals
 
 import octoprint.plugin
 
-class HelloWorldPlugin(octoprint.plugin.StartupPlugin):
-    def on_startup(self):
+class HelloWorldPlugin(octoprint.plugin.StartupPlugin,
+                       octoprint.plugin.TemplatePlugin):
+    def on_after_startup(self):
         self._logger.info("Hello World!")
 
 __plugin_name__ = "Hello World"
